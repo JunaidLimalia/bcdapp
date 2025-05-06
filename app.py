f2cd431ffa1b14c551ac46b238228b41ec450222
Junaid Limalia <mlim0062@student.monash.edu>
2025-05-06 13:33:03 +0800

    test

--- app.py
+++ app.py
@@ -141,4 +141,6 @@ def predict():
 
 if __name__ == "__main__":
     # app.run(debug=True)
-    app.run(debug=True, host="0.0.0.0", port=8080)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port, debug=True)
+    # app.run(debug=True, host="0.0.0.0", port=8080)
