56ea4f6726dac20f6b3bedb1518554c0d41cde4b
Junaid Limalia <mlim0062@student.monash.edu>
2025-05-06 19:09:28 +0800

    fixes

--- app.py
+++ app.py
@@ -150,7 +150,7 @@ def predict():
 #         return send_from_directory(app.static_folder, "index.html")
 
 if __name__ == "__main__":
-    # app.run(debug=True)
-    port = int(os.environ.get("PORT", 8080))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(debug=True)
+    # port = int(os.environ.get("PORT", 8080))
+    # app.run(host="0.0.0.0", port=port, debug=True)
     # app.run(debug=True, host="0.0.0.0", port=8080)
